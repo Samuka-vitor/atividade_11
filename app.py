60879db155ede9031f9a5d39983fd4a3ca66243b
Samuka-vitor <samuelvitor93andrade@gmail.com>
2024-10-11 19:46:08 -0300

    versão 1.0

--- app.py
+++ app.py
@@ -5,11 +5,8 @@ a = float(input("Digite o valor de 'a': "))
 b = float(input("Digite o valor de 'b': "))
 
 def calcular_funcao(a,b):
-    if a != 0:
         x = -b/a
         return x
-    else:
-        return "O valor de 'a' não pode ser zero."
 
 # Chamando a função pra calcular o valor de 'x'
 resultado = calcular_funcao(a,b)
